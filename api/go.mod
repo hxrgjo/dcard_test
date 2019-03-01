fdfb6d8e951991de3fda0157bf9760f0c43be012
ray <mathew177@gmail.com>
2019-03-01 10:12:37 +0800

    Init API server, and add a health check

--- api/go.mod
+++ api/go.mod
@@ -2,7 +2,12 @@ module api
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/gin-contrib/sse v0.0.0-20190226023149-996076df5b33 // indirect
+	github.com/gin-gonic/gin v1.3.0
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/go-xorm/xorm v0.7.1
+	github.com/golang/protobuf v1.3.0 // indirect
+	github.com/mattn/go-isatty v0.0.6 // indirect
 	github.com/spf13/viper v1.3.1
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 )
--- api/main.go
+++ api/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"api/model"
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -11,11 +14,13 @@ var config Config
 func main() {
 	var err error
 
+	// read config.yaml
 	config, err = readConfig()
 	if err != nil {
 		panic("init config error:" + err.Error())
 	}
 
+	// init mysql db connect
 	err = model.InitDB(
 		config.DB.User,
 		config.DB.Password,
@@ -25,4 +30,15 @@ func main() {
 	if err != nil {
 		panic("init db error:" + err.Error())
 	}
+
+	// init api server
+	initServer()
+}
+
+func initServer() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "OK")
+	})
+	r.Run(fmt.Sprintf(":%d", config.Port)) // listen and serve on 0.0.0.0:8080
 }
